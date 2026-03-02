9a7788add87031a74dfb2dd196b437d5928edad9
Nomix17 <izayrit12345678@gmail.com>
2026-03-02 23:49:59 +0000

    fixing some typos

--- encryption.py
+++ encryption.py
@@ -8,21 +8,21 @@ load_dotenv()
 key= os.environ.get('AES_KEY').encode()
 
 
-#encryption fonction
+#encryption function
 def encrypt_password(data):
     cipher=AES.new(key,AES.MODE_CBC)
     iv=cipher.iv
     ciphertext=cipher.encrypt(pad(data.encode(),AES.block_size))
     return iv , ciphertext
 
-#decryption fonciton
+#decryption function 
 def decrypt_password(iv , ciphertext):
     cipher = AES.new(key,AES.MODE_CBC,iv=iv)
     plaintext=unpad(cipher.decrypt(ciphertext),AES.block_size)
     return plaintext.decode()
 
 
-#Password_strength fonction
+#Password_strength function
 def check_Strength(password):
     score = 0
     feedback= []
